50eff97361975f07922512d853f1db90ae3dad8f
Shin-sangwon <dnjsml10@gmail.com>
2023-01-04 22:20:22 +0900

    [Object] feat : 소극장 클래스 구현

---
+++ Object/src/main/java/com/example/object/chapter1/Theater.java
@@ -0,0 +1,27 @@
+package com.example.object.chapter1;
+
+public class Theater {
+
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+    /* 관람객 맞이하기
+    *  소극장은 괌라객의 가방 안에 초대장이 들어 있는지 확인한다.
+    *  만약 초대장이 들어 있다면 이벤트에 당첨된 관람객이므로 판매원에게서 받은 티켓을 관람객의 가방 안에 넣어준다.
+    *  가방 안에 초대장이 없다면 티켓을 판매해야 한다.
+    * */
+    public void Enter(Audience audience) {
+        if(audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+
+    }
+}
